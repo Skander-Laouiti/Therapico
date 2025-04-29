03a987dd2da4f80dc239e73f2a027b9c39115099
unknown <mahox.gt@gmail.com>
2025-04-29 01:48:33 +0100

    Patient Management

---
+++ Model/patient.php
@@ -0,0 +1,115 @@
+<?php
+class patient
+{
+    private $cin_p;
+    private $nom_prenom_p ;
+    private $date_n_p;
+    private  $tel_p;
+    private $email_p;
+    private $sexe_p;
+    private  $pass_p;
+    
+
+    public function __construct($cin_p, $nom_prenom_p, $date_n_p, $tel_p, $email_p,$sexe_p,$pass_p)
+    {
+        $this->cin_p = $cin_p;
+        $this->nom_prenom_p = $nom_prenom_p;
+        $this->date_n_p = $date_n_p;
+        $this->tel_p = $tel_p;
+        $this->email_p = $email_p;
+        $this->sexe_p = $sexe_p;
+        $this->pass_p = $pass_p;
+    }
+
+    /**
+     * Get the value of cin p
+     */
+    public function get_cin_p()
+    {
+        return $this->cin_p;
+    }
+
+    public function setcin_p($cin_p)
+    {
+        $this->cin_p = $cin_p;
+
+        return $this;
+    }
+   
+    public function getnom_prenom_p()
+    {
+        return $this->nom_prenom_p;
+    }
+
+    public function setnom_prenom_p($nom_prenom_p)
+    {
+        $this->nom_prenom_p = $nom_prenom_p;
+
+        return $this;
+    }
+
+   
+    public function getemail_p()
+    {
+        return $this->email_p;
+    }
+
+  
+    public function setemail_p($email_p)
+    {
+        $this->email_p = $email_p;
+
+        return $this;
+    }
+
+    
+    public function getdate_n_p()
+    {
+        return $this->date_n_p;
+    }
+
+   
+    public function setdate_n_p($date_n_p)
+    {
+        $this->date_n_p = $date_n_p;
+
+        return $this;
+    }
+
+    public function gettel_p()
+    {
+        return $this->tel_p;
+    }
+
+    public function settel_p($tel_p)
+    {
+        $this->tel_p = $tel_p;
+
+        return $this;
+    }
+
+    public function getsexe_p()
+    {
+        return $this->sexe_p;
+    }
+
+    public function setsexe_p($sexe_p)
+    {
+        $this->sexe_p= $sexe_p;
+
+        return $this;
+    }
+
+    public function getpass_p()
+    {
+        return $this->pass_p;
+    }
+
+    public function setpass_p($pass_p)
+    {
+        $this->pass_p = $pass_p;
+
+        return $this;
+    }
+
+}
